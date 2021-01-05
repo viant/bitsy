ea7cb6bfa8b01f055c5e8df82d145be98c921f15
vcarey <val@adelphic.com>
2021-01-05 11:22:11 -0500

    Minor CLI change

--- cmd/bitsy/app.go
+++ cmd/bitsy/app.go
@@ -4,14 +4,17 @@ import (
 	_ "github.com/viant/afsc/s3"
 	"github.com/viant/bitsy/cmd"
 	_ "net/http/pprof"
+	"os"
 )
 
-var Version string
+var Version string = "1.0"
 
 func main() {
-	args := []string{
-		"app", "-r=/Users/ppoudyal/go/src/github.com/viant/bitsy/e2e/regression/cases/02_multibatch/rule/rule.yaml", "-s=/Users/ppoudyal/go/src/github.com/viant/bitsy/e2e/regression/cases/02_multibatch/data/trigger/input.json",
-	}
+	//args := []string{
+	//	"app", "-r=/Users/ppoudyal/go/src/github.com/viant/bitsy/e2e/regression/cases/02_multibatch/rule/rule.yaml", "-s=/Users/ppoudyal/go/src/github.com/viant/bitsy/e2e/regression/cases/02_multibatch/data/trigger/input.json",
+	//}
+
+	args := os.Args
 	cmd.RunClient(Version, args)
 
 }
