18340ca423c32b21cde9e99670634cf3cce3dcd4
Adrian Witas <adrianwit@gmail.com>
2021-04-08 18:31:29 -0700

    added no found error handling

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/viant/afs"
+	"github.com/viant/afs/option"
 	"github.com/viant/cloudless/data/processor/adapter/gcp"
 )
 
@@ -15,10 +16,15 @@ func HandleEvent(ctx context.Context, event gcp.GSEvent) error {
 		fmt.Printf("could not init service: %v\n", err)
 		return nil
 	}
+	ok, _ := service.fs.Exists(ctx, event.URL(), option.NewObjectKind(true))
+	if ! ok {
+		fmt.Printf(`{"Status":"noFound", "URL":"%v"}`, event.URL())
+		return nil
+	}
 	request, err := event.NewRequest(ctx, fs, &service.config.Config)
 	if err != nil {
-		if ok,_ :=  fs.Exists(ctx, event.URL()); ok {
-			return nil;
+		if ok, _ := fs.Exists(ctx, event.URL()); ok {
+			return nil
 		}
 		return fmt.Errorf("failed to build request: %w", err)
 	}
