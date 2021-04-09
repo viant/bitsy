1fd5ce78411a4f4562333010780054dea5a37f04
Adrian Witas <adrianwit@gmail.com>
2021-04-08 18:41:09 -0700

    added no found error handling

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -16,14 +16,10 @@ func HandleEvent(ctx context.Context, event gcp.GSEvent) error {
 		fmt.Printf("could not init service: %v\n", err)
 		return nil
 	}
-	ok, _ := service.fs.Exists(ctx, event.URL(), option.NewObjectKind(true))
-	if ! ok {
-		fmt.Printf(`{"Status":"noFound", "URL":"%v"}`, event.URL())
-		return nil
-	}
 	request, err := event.NewRequest(ctx, fs, &service.config.Config)
 	if err != nil {
-		if ok, _ := fs.Exists(ctx, event.URL()); ok {
+		if ok, _ := fs.Exists(ctx, event.URL(), option.NewObjectKind(true)); ! ok {
+			fmt.Printf(`{"Status":"noFound", "URL":"%v"}`, event.URL())
 			return nil
 		}
 		return fmt.Errorf("failed to build request: %w", err)
